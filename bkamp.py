15b851efb6e13229c6a0e2a0bba1e33ff841a380
root <root@localhost.localdomain>
2019-03-07 21:33:09 +0800

    testing out proper git lmao

--- bkamp.py
+++ bkamp.py
@@ -21,6 +21,6 @@ def arrayChoice(n):
     return array 
 
 def commonPasswords:
-    array = ["password", "123456", "123456", "1234", "qwerty", "12345", "dragon", "pussy", "baseball", "football", "letmein", "monkey", "696969", "abc123", "mustang", "michael", "shadow", "master", "jennifer", "111111", "2000", "jordan", "superman", "harley", "1234567", "fuckme", "hunter", "fuckyou", "trustno1"]
+    array = ["password", "123456", "123456", "1234", "qwerty", "12345", "dragon", "pussy", "baseball", "football", "letmein", "monkey", "696969", "abc123", "mustang", "michael", "shadow", "master", "jennifer", "111111", "2000", "jordan", "superman", "harley", "1234567", "fuckme", "hunter", "fuckyou", "trustno1","ranger", "buster"]
     return array
     #source: https://www.passwordrandom.com/most-popular-passwords
